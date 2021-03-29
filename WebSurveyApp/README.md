9b0a30bbec39008d9e2c89a96d1b2e74629c3361
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2021-03-29 03:16:07 +0300

    course-project-1: added WebSurveyApp

---
+++ WebSurveyApp/README.md
@@ -0,0 +1,19 @@
+﻿<h1>WebSurveyApp</h1>
+
+This is my own course project,
+which became the final stage of the .Net speciality.
+
+Technology used:</br>
+🔹C#</br>
+🔹ASP.NET Core (MVC)</br>
+🔹Entity Framework Core</br>
+🔹HTML</br>
+🔹CSS</br>
+ </br>
+<h2>Demo</h2>
+<img src="https://j.gifs.com/VA8Gw5.gif"/>
+</br>
+</br>
+
+Here is the link on YouTube: https://www.youtube.com/watch?v=Idc-shdlM_A
+
---
+++ WebSurveyApp/WebLogic/Services/SurveyService.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebLogic.Services
+{
+    public class SurveyService
+    {
+        private readonly SurveyDbContext _context;
+
+        public SurveyService(SurveyDbContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateSurveyAsync(Survey surveyModel, string email)
+        {
+            var userModel = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+            userModel.Surveys.Add(surveyModel);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditSurveyAsync(Survey surveyModel)
+        {
+            _context.Update(surveyModel);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSurveyAsync(int surveyId)
+        {
+            var surveyModel = await _context.Surveys
+                 .Include("Reports")
+                 .Include("Reports.Responses")
+                 .FirstOrDefaultAsync(r => r.Id == surveyId);
+
+            if (surveyModel != null)
+            {
+                var responses = surveyModel.Reports.SelectMany(x => x.Responses);
+
+                if (responses != null)
+                {
+                    _context.Responses.RemoveRange(responses);
+                }
+
+                _context.Remove(surveyModel);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Survey>> GetSurveysAsync(string email)
+        {
+            var surveys = await _context.Surveys
+                .Include(s => s.Reports)
+                .Where(s => s.User.Email == email)
+                .ToListAsync();
+
+            return surveys;
+        }
+
+        public async Task<Survey> GetSurveyById(int surveyId)
+        {
+            var survey = await _context.Surveys
+                .Include(s => s.Questions)
+                .ThenInclude(q => q.Options)
+                .FirstOrDefaultAsync(s => s.Id == surveyId);
+
+            return survey;
+        }
+    }
+}
